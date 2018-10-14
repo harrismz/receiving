e1c3b6e9da4ac4329fc630fbd53dff9efc04f76b
Harris Muhammad <harris.git21@gmail.com>
2018-10-14 19:47:42 +0700

    Old Receiving Part 4 ( folder double SO upload )

---
+++ double SO upload/frmupload2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head>
+<title>FORM SO DATA UPLOAD</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body class="body1"><a href="index.php">menu</a>
+<br /><br />
+<form method="post" action="upload3.php" ENCTYPE="multipart/form-data">Upload SO DATA (SA90....csv) --- TEST<br />
+<input type="file" name="userfile">
+<input type="submit" value="Upload">
+<input type="hidden" name="loadheader" value="ok">
+</form>
+<!-- <form method="post" action="upload_sa90prod.php" ENCTYPE="multipart/form-data">Upload SO DATA to sa90prod (SA90....csv)<br /><input type="file" name="userfile"><input type="submit" value="Upload"><input type="hidden" name="loadheader" value="ok"></form> -->
+
+</body>
+</html>
